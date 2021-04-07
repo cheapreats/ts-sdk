44c16525748b409428263b2263f6ea9045aba0e1
DenisMela <MelamedDenis@hotmail.com>
2021-04-07 15:43:42 -0400

    isSplitPaying update

--- src/app/controllers/CartController.ts
+++ src/app/controllers/CartController.ts
@@ -45,6 +45,7 @@ export interface Cart extends DefaultControllerRequired {
   total: number;
   coupons: Array<CartCoupon>;
   note: string;
+  isSplitPaying: boolean;
   participating_customers: Array<ParticipatingCustomer>;
 }
 
